57244c6a40296013df47be1bddba7e78965805d5
Mateusz Kloczko <mkloczko@student.agh.edu.pl>
2012-11-30 21:10:20 +0100

    Added admin's control panel, and verification of cities.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,14 @@
+import erasm_proj_agh.City
+
 class BootStrap {
 
     def init = { servletContext ->
-    }
+    
+		if (!City.count()) {
+			new City(name: "Kraków", description: "", confirmed:true).save(failOnError: true)
+			new City(name: "Oooooo", description: "Another universe", confirmed:false).save(failOnError: true)
+		}
+	}
     def destroy = {
     }
 }
