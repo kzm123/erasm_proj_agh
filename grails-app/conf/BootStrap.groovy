abc0f62701b09028dfc4447c33289bb9a8820278
Mateusz Kloczko <mkloczko@student.agh.edu.pl>
2012-11-30 22:35:42 +0100

    Wyczyscilem BootStrap.groovy by nie ladowal na starcie danych do baz danych.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,13 +1,9 @@
 import erasm_proj_agh.City
+import erasm_proj_agh.Country
 
 class BootStrap {
 
     def init = { servletContext ->
-    
-		if (!City.count()) {
-			new City(name: "Kraków", description: "", confirmed:true).save(failOnError: true)
-			new City(name: "Oooooo", description: "Another universe", confirmed:false).save(failOnError: true)
-		}
 	}
     def destroy = {
     }
